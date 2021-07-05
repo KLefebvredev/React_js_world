70ac216c5fabc06e920b953b81ff6047bea6cefb
klefebvre <klefebvre.dev@gmail.com>
2021-07-05 16:16:08 +0200

    V.1 site cours reactJs

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>À propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias minima vitae ab magni voluptatibus enim, quisquam a dolore quam voluptas perferendis quo voluptates illo eaque magnam. Vero sunt quos laudantium sapiente distinctio? Cupiditate, ratione asperiores nostrum qui dolorem, quisquam suscipit ex, dolorum eius eos odio quaerat velit enim animi beatae in blanditiis aut repellendus cum consequatur magnam placeat reprehenderit.</p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor impedit deserunt asperiores dolore fuga. Natus, quis! Voluptas quos deleniti maxime qui nulla tempore, fugit asperiores dolorum non, omnis necessitatibus consectetur aperiam esse voluptatibus iure. Sequi, beatae velit officia et voluptatum ab quaerat modi fugit est debitis omnis voluptatem cum ut voluptatibus laudantium vero quis nemo quas soluta nam autem excepturi repellat illo. Recusandae, accusantium! Quos, quia. Tempore optio nihil veritatis dignissimos earum odit facilis voluptas blanditiis rerum at ut sed dolore nobis, molestias similique velit, iusto est dicta assumenda perspiciatis alias? Quas porro distinctio explicabo officiis, praesentium id quam laudantium perspiciatis aspernatur, eveniet corrupti ab! Velit eius, enim vitae quas incidunt, saepe totam excepturi nihil maiores aspernatur porro quaerat ullam nisi, in veniam ratione veritatis numquam. Pariatur quis rem cupiditate, veritatis, explicabo neque accusamus aut, harum recusandae ipsa blanditiis quae.
+            </p>
+        </div>
+    );
+};
+
+export default About;
